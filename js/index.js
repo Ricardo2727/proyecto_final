9cbfba5f43917fb323de41da420e77f7a5a1c28f
RicardoValenzuela <valenzuelaopazor@gmail.com>
2024-01-17 00:23:37 -0300

    js index clubkop 2

--- js/index.js
+++ js/index.js
@@ -9,45 +9,72 @@ const BBDD = [
         nombre: "Sol",
         usuario: "s@gmail.com",
         contraseña: "456",
-        email: "s@gmail,com"
+        email: "s@gmail.com"
     }
 ]
 
 const botonIniciar = document.querySelector("#btn-iniciar");
 const contenedor = document.querySelector("#contenedorUsuario");
 const inputs = document.querySelectorAll("input");
+const contenedor2 = document.querySelector("#userOffLog");
+const isLog = JSON.parse(localStorage.getItem("isLog")) ?? {}
 
-const isLog = JSON.parse (localStorage.getItem ("isLog")) ?? {}
+const pintar = (nombre) => {
+    contenedor.innerHTML = `<h2 id="contenedorUsuario" class="colorCrema tamGrande focus-in-contract-bck"> ${nombre}, Bienvenido a Club KOP nuevamente!</h2> 
+                            <ul class="clubkop__padNone">
+                            <li>
+                                <div class="boton txtBoton vibrate-1 ">
+                                    <a class="colorCrema" href="./paginas/clubkop.html">IR A CLUB KOP</a>
+                                </div>
+                            </li>
+                            </ul>`}
+isLog.usuario && pintar(isLog.usuario)
 
-const pintar = (nombre) => {contenedor.innerHTML = `<h2 id="contenedorUsuario" class="colorCrema posH2"> ${nombre}, Bienvenido a Club KOP nuevamente!</h2>`}
-isLog.usuario && pintar(" " + isLog.usuario)
-    
+console.log(isLog.usuario);
+if (isLog.usuario === undefined) {
+    if (contenedor2) {
+        contenedor2.innerHTML = `<div id="userOffLog"></div>`;
+    } else {
+        console.error("El contenedor2 no se encontró en el DOM");
+    }
+}
+
 
 const usuarioALoguear = {
-    usuario:'',
+    usuario: '',
     password: '',
-    confirmarPassword: ''
+    confirmarPassword: '',
+    nombre: '',
+    email: ''
+    
 }
 
 inputs.forEach((input) => {
-    input.addEventListener("input", (event)=>{
-        const {name, value} = event.target
+    input.addEventListener("input", (event) => {
+        const { name, value } = event.target
         usuarioALoguear[name] = value
-        
+
     })
 })
 
 botonIniciar.addEventListener("click", () => {
-    
-    const siExiste = BBDD.find((persona)=> {
+
+    const siExiste = BBDD.find((persona) => {
         return persona.usuario === usuarioALoguear.usuario && persona.contraseña === usuarioALoguear.password
     })
-    
-    if(siExiste !== undefined){
-        localStorage.setItem("isLog", JSON.stringify({usuario:usuarioALoguear.usuario}))
+
+    if (siExiste !== undefined) {
+        localStorage.setItem("isLog", JSON.stringify({ usuario: usuarioALoguear.usuario }))
         console.log(usuarioALoguear);
-        contenedor.innerHTML =  `<h2 id="contenedorUsuario" class="colorCrema posH2"> ${siExiste.usuario}, Bienvenido a Club KOP nuevamente!</h2>`
-    }else{
+        contenedor.innerHTML = `<h2 id="contenedorUsuario" class="colorCrema tamGrande focus-in-contract-bck"> ${siExiste.usuario}, Bienvenido a Club KOP nuevamente!</h2> 
+                            <ul class="clubkop__padNone">
+                            <li>
+                                <div class="boton txtBoton vibrate-1 ">
+                                    <a class="colorCrema" href="./paginas/clubkop.html">IR A CLUB KOP</a>
+                                </div>
+                            </li>
+                            </ul>`
+    } else {
         console.log("Usuario no registrado")
     }
 
